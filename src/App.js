50b782512da2178dd888e9966ca165d783bffcc7
allencpu <allenproof@yahoo.com>
2024-09-15 22:15:25 +0000

    Added time+date feature to Hello app.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(props) {
+  const currDate = new Date();
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1> Allen says, 'Hello'.</h1>
+        <h2> The present time is now {currDate.toLocaleTimeString()}.</h2>
+        <h2> The date is {currDate.toLocaleDateString()}.</h2>
         <a
           className="App-link"
           href="https://reactjs.org"
